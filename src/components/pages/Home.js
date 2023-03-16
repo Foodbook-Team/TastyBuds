dc99ff3865786f2d4508776de34975dd33ddd16d
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-03-16 16:48:09 +0000

    adding api to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,27 @@
 import React from "react";
 // import Nav from '../Nav';
 
+  
 
 function Home() {
+  
+  // const APP_ID = "31e49968"
+  // const APP_KEY = "ecdd8eae17634d382403cbce72038924"
+
+  // const url = `/api/recipes/v2/?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&type=public`;
+  
+  // const getData = async () => {
+  //   const response = await fetch(url);
+  //   const result = await response.json();
+  //   console.log(result);
+    
+  
+  
   return (
     <div>    
-      
+      {/* Inspiration: <img src={result.hits[0].recipe.images.REGULAR.url} alt="jumbotron"></img> */}
+      Home
+
     </div>
     
   );
