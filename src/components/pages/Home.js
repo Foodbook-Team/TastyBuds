51347292759cedc23a6b03847e42b8c0f84a9d4b
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-03-21 12:58:41 +0000

    commitment

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,19 +3,7 @@ import React from "react";
 
   
 
-function Home() {
-  
-  // const APP_ID = "31e49968"
-  // const APP_KEY = "ecdd8eae17634d382403cbce72038924"
-
-  // const url = `/api/recipes/v2/?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&type=public`;
-  
-  // const getData = async () => {
-  //   const response = await fetch(url);
-  //   const result = await response.json();
-  //   console.log(result);
-    
-  
+function Home() {   
   
   return (
     <div>    
