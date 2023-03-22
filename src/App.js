60d07bcd6face34e2a2f3f978136a179f5594f32
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-03-22 12:04:24 +0000

    initial render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavTabs from './components/NavTabs';
 import Home from './components/pages/Home';
@@ -8,21 +8,13 @@ import Page3 from './components/pages/Page3';
 import MediaCard from "./components/MediaCard";
 import Footer from "./components/Footer";
 import Jumbotron from './components/Jumbotron';
-import axios from "axios"
+
 
 
 function App() {
-  const [mealOfDay, setMealOfDay] = useState(); 
   
-  useEffect(()=>{    
-    const MealOfDay = async () => {
-      const result = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&app_id=31e49968&app_key=ecdd8eae17634d382403cbce72038924&diet=balanced&health=alcohol-free&mealType=Lunch&imageSize=LARGE&random=true`);      
-      setMealOfDay(result.data.hits)
-      console.log(result);
-    }
-    MealOfDay();
+
     
-  },[]);
   return (
   // -------------Start of div container-----------------
   <>
@@ -37,8 +29,8 @@ function App() {
         </Routes>        
       </div>
     </Router>
-    <Jumbotron />
-
+    {/* <Jumbotron /> */}
+    
     {/* <MediaCard /> */}
     <Footer /> 
   
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,81 @@
-import React from "react";
-// import Nav from '../Nav';
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid'
+import Item from '@mui/material/ListItem'
+import { CardActionArea } from '@mui/material';
+import axios from "axios"
+import bootstrap from "bootstrap"
+import './Home.css'
 
-  
 
-function Home() {   
-  
+
+export default function Home() {
+  const [mealOfDay, setMealOfDay] = useState();
+  const [mealOfDayImage, setMealOfDayImage] = useState();
+  const [link, setLink] = useState();
+
+
+  useEffect(() => {
+    const MealOfDay = async () => {
+      const result = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&app_id=31e49968&app_key=ecdd8eae17634d382403cbce72038924&diet=balanced&health=alcohol-free&mealType=Lunch&imageSize=LARGE&random=true`);
+      setMealOfDay(result.data.hits[0].recipe.label);
+      setMealOfDayImage(result.data.hits[0].recipe.images.LARGE.url)
+      setLink(result.data.hits[0].recipe.images.url)
+      
+    }
+
+    MealOfDay();
+
+  }, []);
+
   return (
-    <div>    
-      {/* Inspiration: <img src={result.hits[0].recipe.images.REGULAR.url} alt="jumbotron"></img> */}
-      Home
-
-    </div>
+    <>    
+    <div className="HomeCard">
     
+    <img src={`${mealOfDayImage}`} className="HomeCardImg"></img>
+    <h1>Feel inspired by</h1>
+    <a href={`${link}`}><h2>{mealOfDay}</h2></a>
+    </div>
+      {/* <Grid container 
+            spacing={2}           
+            direction="row"
+            justifyContent="center"
+            alignItems="center" > */}
+            
+        
+          {/* <Card className="InnitialDisplay" sx={{ maxWidth: 345 }} >
+            <CardActionArea>
+              <CardMedia
+                height="100%"
+                component="img"
+                image={mealOfDayImage}
+                alt={mealOfDay}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h4" component="div">
+                <a href="https://www.eatingwell.com/recipe/249819/baby-tiramisu/">{mealOfDay}</a> 
+                </Typography>
+                {/* <Typography variant="body2" color="text.secondary">
+            Description of dish
+          </Typography> */}
+              {/* </CardContent>
+            </CardActionArea> */}
+          {/* </Card> */}
+
+
+        
+
+      {/* </Grid> */} 
+
+
+    </>
+
   );
 }
 
-export default Home;
+
 
 
